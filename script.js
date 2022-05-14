f3d9342f2722925d5413444cd8286f441999d9f3
Zurishaddai Temitope <temitopezurishaddai@gmail.com>
2022-05-14 09:45:18 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -6,7 +6,10 @@ function sendEmail(){
         To : 'temitopezurishaddai@gmai.com',
         From : document.getElementById('email').value,
         Subject : 'New Complain',
-        Body : "And this is the body"
+        Body : 'Name: ' + document.getElementById('name').value
+                + '<br> Email: ' + document.getElementById('email').value
+                + '<br> Phone number: ' + document.getElementById('phone').value
+                + '<br> Complain: ' + document.getElementById('message').value
     }).then(
       message => alert(message)
     );
